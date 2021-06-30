587803e815dcf0125117a0d4308ebd3418c8dbc4
AbrarAlzubaidi <>
2021-06-30 13:18:55 +0000

    update js file

--- colorjs.js
+++ colorjs.js
@@ -1,1 +1,31 @@
+var username
+username=prompt("Please can you enter your name ^_^")
 
+while(!username)
+{
+  alert("i'll appreciate if you enter your name to welcome you ^_^")
+  username=prompt("Please can you enter your name ^_^") 
+    console.log(username)
+
+}
+ alert(" welcome to this website ^_^ " +username )
+  console.log(username)
+
+  var colorName=prompt("could you tell me what is your favorite color")
+  
+while(!colorName)
+{
+  alert(":( oh so you won't to till me your favorite color, but i'll appreciate if you tell me ^_^")
+  colorName=prompt("could you tell me what is your favorite color")
+}
+  if(colorName=="blue")
+  {
+     alert(" Oh really, it is my favorite too have a nice read ^_^ " + username )
+     console.log(username+" likes  "+colorName)
+
+  }
+  else{
+    alert(" your favorite color is "+colorName+" , that's intresting  " +username +", have a nice read ^_^" )
+         console.log(username+" likes  "+colorName)
+
+  }
